58f2dd8a17921cda7c7289dc5379797679094a9c
Piyush Saini <2003piyushsaini@gmail.com>
2026-01-11 23:17:15 +0530

    Add files via upload

---
+++ check 1to10natural no.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i, sum = 0;
+
+    for(i = 1; i <= 10; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum of natural numbers from 1 to 10 = %d", sum);
+
+    return 0;
+}
