b008d8151ec16df204412cded36ec8633d971ddd
Piyush Saini <2003piyushsaini@gmail.com>
2026-01-19 22:13:57 +0530

    Add files via upload

---
+++ Count words in a string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, words = 1;
+
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++)
+        if(str[i] == ' ')
+            words++;
+
+    printf("Words = %d", words);
+    return 0;
+}
