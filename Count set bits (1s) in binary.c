8f71e5a8f805af35e55b6b87459d8436312f8b2a
Piyush Saini <2003piyushsaini@gmail.com>
2026-02-02 23:18:42 +0530

    Add files via upload

---
+++ Count set bits (1s) in binary.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, count=0;
+    scanf("%d",&n);
+
+    while(n>0) {
+        count += n & 1;
+        n >>= 1;
+    }
+
+    printf("Set Bits = %d", count);
+    return 0;
+}
