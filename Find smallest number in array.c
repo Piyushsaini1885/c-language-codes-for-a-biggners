013723ca23a85f50a7c951e81e775001c517bb19
Piyush Saini <2003piyushsaini@gmail.com>
2026-02-11 13:33:07 +0530

    Add files via upload

---
+++ Find smallest number in array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a[5], min;
+    for(int i=0;i<5;i++)
+        scanf("%d",&a[i]);
+
+    min = a[0];
+
+    for(int i=1;i<5;i++)
+        if(a[i] < min)
+            min = a[i];
+
+    printf("Smallest = %d", min);
+    return 0;
+}
