aeb5fc9c461ab82d49a297532084665376238e4b
Piyush Saini <2003piyushsaini@gmail.com>
2026-02-14 14:58:55 +0530

    Add files via upload

---
+++ Insert Element in Array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a[10] = {1,2,3,4,5};
+    int n=5, pos=2, value=99;
+
+    for(int i=n;i>=pos;i--)
+        a[i] = a[i-1];
+
+    a[pos-1] = value;
+    n++;
+
+    for(int i=0;i<n;i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
