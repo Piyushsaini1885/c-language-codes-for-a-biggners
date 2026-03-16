c7a6dac32be1612cd162c226e1524b4655362fe3
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-16 22:53:15 +0530

    Add files via upload

---
+++ Check Prime (Using Function).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int prime(int n) {
+    int i;
+
+    for(i=2;i<=n/2;i++) {
+        if(n%i==0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d",&num);
+
+    if(prime(num))
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
