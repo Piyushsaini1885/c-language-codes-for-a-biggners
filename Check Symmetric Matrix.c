ea18e156d36a744a369cf30d4de4bc35e4a126cf
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-22 20:23:27 +0530

    Add files via upload

---
+++ Check Symmetric Matrix.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], i, j, n, flag=1;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+            if(a[i][j] != a[j][i])
+                flag=0;
+        }
+    }
+
+    if(flag)
+        printf("Symmetric Matrix");
+    else
+        printf("Not Symmetric");
+
+    return 0;
+}
