7e20e947f7ffc4d3932b4640d7d7d9576913ea70
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-23 20:24:03 +0530

    Add files via upload

---
+++ Transpose Matrix (In-place).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], i, j, n, temp;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++) {
+        for(j=i+1;j<n;j++) {
+            temp = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = temp;
+        }
+    }
+
+    printf("Transpose:\n");
+
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
