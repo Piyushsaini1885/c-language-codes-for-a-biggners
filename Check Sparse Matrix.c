8d4e2a724f9752a148616509acffc5c5e6120964
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-23 20:25:05 +0530

    Add files via upload

---
+++ Check Sparse Matrix.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], i, j, r, c, zero=0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            if(a[i][j]==0)
+                zero++;
+
+    if(zero > (r*c)/2)
+        printf("Sparse Matrix");
+    else
+        printf("Not Sparse");
+
+    return 0;
+}
