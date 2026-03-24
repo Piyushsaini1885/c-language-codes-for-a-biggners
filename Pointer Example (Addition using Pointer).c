25cb74bfdc1ced4f7fb92ad5a97b0de4e6d97fa3
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-24 16:57:11 +0530

    Add files via upload

---
+++ Pointer Example (Addition using Pointer).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a=10, b=20, sum;
+    int *p1, *p2;
+
+    p1 = &a;
+    p2 = &b;
+
+    sum = *p1 + *p2;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
