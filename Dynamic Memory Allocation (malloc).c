d01703bc00bcb937375fd877e2e8294b963ea11f
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-25 18:30:15 +0530

    Add files via upload

---
+++ Dynamic Memory Allocation (malloc).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *p, n, i;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    p = (int*) malloc(n * sizeof(int));
+
+    for(i=0;i<n;i++)
+        scanf("%d",&p[i]);
+
+    printf("Elements:\n");
+
+    for(i=0;i<n;i++)
+        printf("%d ",p[i]);
+
+    free(p);
+
+    return 0;
+}
