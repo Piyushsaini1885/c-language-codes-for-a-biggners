788c113a6303f62d403147b2d0f709c1bcba512d
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-25 18:31:10 +0530

    Add files via upload

---
+++ Simple Stack (Push + Pop).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int stack[100], top=-1;
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+void pop() {
+    if(top==-1)
+        printf("Stack Underflow\n");
+    else
+        printf("Popped = %d\n", stack[top--]);
+}
+
+int main() {
+    push(10);
+    push(20);
+    pop();
+
+    return 0;
+}
