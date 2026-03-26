38dcb27f69d6d705dde6ed54ca67051db3de2e2c
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-26 16:11:31 +0530

    Add files via upload

---
+++ Singly Linked List (Insert at Beginning).c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *head=NULL;
+
+void insert(int x) {
+    struct node *newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+
+    newnode->data = x;
+    newnode->next = head;
+    head = newnode;
+}
+
+void display() {
+    struct node *temp=head;
+
+    while(temp!=NULL) {
+        printf("%d ", temp->data);
+        temp=temp->next;
+    }
+}
+
+int main() {
+    insert(10);
+    insert(20);
+    insert(30);
+    display();
+
+    return 0;
+}
