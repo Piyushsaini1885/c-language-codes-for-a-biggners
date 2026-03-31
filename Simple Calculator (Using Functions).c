c1dceb787019d038da9598316612fee2794f5f1d
Piyush Saini <2003piyushsaini@gmail.com>
2026-03-31 08:50:15 +0530

    Add files via upload

---
+++ Simple Calculator (Using Functions).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int add(int a,int b){ return a+b; }
+int sub(int a,int b){ return a-b; }
+int mul(int a,int b){ return a*b; }
+
+int main() {
+    int a,b;
+
+    scanf("%d%d",&a,&b);
+
+    printf("Add=%d\n",add(a,b));
+    printf("Sub=%d\n",sub(a,b));
+    printf("Mul=%d\n",mul(a,b));
+
+    return 0;
+}
