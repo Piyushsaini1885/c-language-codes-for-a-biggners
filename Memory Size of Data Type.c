a03fd1b1a889f558da3dcd14c70471bfa0d145aa
Piyush Saini <2003piyushsaini@gmail.com>
2026-04-05 13:00:33 +0530

    Add files via upload

---
+++ Memory Size of Data Type.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("int=%lu\n", sizeof(int));
+    printf("float=%lu\n", sizeof(float));
+    printf("char=%lu\n", sizeof(char));
+    printf("double=%lu\n", sizeof(double));
+
+    return 0;
+}
