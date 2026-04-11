082c07306bb5b5845d85ab1f49418c15a7a9a96f
Piyush Saini <2003piyushsaini@gmail.com>
2026-04-11 17:40:44 +0530

    Add files via upload

---
+++ Sum of Boundary Elements (Matrix).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r,c,i,j,sum=0;
+
+    scanf("%d%d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<r;i++) {
+        for(j=0;j<c;j++) {
+            if(i==0 || j==0 || i==r-1 || j==c-1)
+                sum += a[i][j];
+        }
+    }
+
+    printf("Boundary Sum=%d",sum);
+
+    return 0;
+}
