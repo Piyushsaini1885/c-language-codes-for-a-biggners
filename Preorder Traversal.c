2f4aa351b6e8d67a573cd479014afd8fc5b4dbad
Piyush Saini <2003piyushsaini@gmail.com>
2026-06-30 23:35:27 +0530

    Delete Preorder Traversal.c

--- Preorder Traversal.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void preorder(struct node* root) {
-    if(root!=NULL) {
-        printf("%d ", root->data);
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
