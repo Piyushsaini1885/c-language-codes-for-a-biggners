842d4da2f5c57fe68ffac2f4147b2ab696e24116
Piyush Saini <2003piyushsaini@gmail.com>
2026-07-11 15:45:05 +0530

    Delete Postorder Traversal.c

--- Postorder Traversal.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void postorder(struct node* root) {
-    if(root!=NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
-}
