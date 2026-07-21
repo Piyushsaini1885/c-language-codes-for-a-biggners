9df72dbf8c90116214faa8ab8171d484f536607c
Piyush Saini <2003piyushsaini@gmail.com>
2026-07-21 23:19:15 +0530

    Delete Inorder Traversal (Binary Tree).c

--- Inorder Traversal (Binary Tree).c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void inorder(struct node* root) {
-    if(root!=NULL) {
-        inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
-    }
-}
