350692c0dd0a91252d798efe6a6be9bfa8064de9
Piyush Saini <2003piyushsaini@gmail.com>
2026-08-30 23:21:06 +0530

    Add files via upload

---
+++ Check Substring.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], sub[50];
+
+    gets(str);
+    gets(sub);
+
+    if(strstr(str,sub))
+        printf("Substring Found");
+    else
+        printf("Not Found");
+
+    return 0;
+}
