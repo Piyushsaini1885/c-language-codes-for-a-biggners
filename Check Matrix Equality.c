f039f7d589006ed92430ee34b235644324098ce5
Piyush Saini <2003piyushsaini@gmail.com>
2026-08-30 23:20:06 +0530

    Add files via upload

---
+++ Check Matrix Equality.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], r,c,i,j,flag=1;
+
+    scanf("%d%d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&b[i][j]);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++) {
+            if(a[i][j]!=b[i][j]) {
+                flag=0;
+                break;
+            }
+        }
+
+    if(flag)
+        printf("Equal");
+    else
+        printf("Not Equal");
+
+    return 0;
+}
