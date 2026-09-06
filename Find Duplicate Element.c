c8a18261b1287162d0011b1ec2e79d559b647b0f
Piyush Saini <2003piyushsaini@gmail.com>
2026-09-06 23:18:03 +0530

    Add files via upload

---
+++ Find Duplicate Element.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    for(i=0;i<n;i++) {
+        for(j=i+1;j<n;j++) {
+            if(arr[i]==arr[j])
+                printf("Duplicate=%d\n",arr[i]);
+        }
+    }
+
+    return 0;
+}
